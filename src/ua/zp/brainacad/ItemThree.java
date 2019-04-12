0a8b742208077f8fc7366430cf8da3e974293a0c
n1ghtmare88 <dmitriy.zp.city@gmail.com>
2019-04-12 09:52:49 +0300

    второй класс

---
+++ src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,12 @@
+/**
+ * @autor Дмитрий Евтушенко
+ * @version 1.0
+ * Этот класс является демонстацией управляющих последовательностей
+ */
+package ua.zp.brainacad;
+
+public class ItemThree {
+    public static void main(String[] args) {
+        System.out.println("H\ne\nl\nl\no\nW\to\tr\tl\td");
+    }
+}
