c498926c410965ce0e98258dfa375699bb8a1aa2
n1ghtmare88 <dmitriy.zp.city@gmail.com>
2019-04-12 09:53:15 +0300

    третий класс

---
+++ src/ua/zp/brainacad/ItemTwo.java
@@ -0,0 +1,12 @@
+/**
+ * @autor Дмитрий Евтушенко
+ * @version 1.0
+ * Этот класс выводит в консоль автора этого проэкта
+ */
+package ua.zp.brainacad;
+
+public class ItemTwo {
+    public static void main(String[] args) {
+        System.out.println("Dmitriy Yevtushenko");
+    }
+}
