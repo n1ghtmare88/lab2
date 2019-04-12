2b9f09c16749506ca920e7937efddaa112064971
n1ghtmare88 <dmitriy.zp.city@gmail.com>
2019-04-12 09:51:47 +0300

    первый класс

---
+++ src/ua/zp/brainacad/ItemOne.java
@@ -0,0 +1,12 @@
+/**
+ * @autor Дмитрий Евтушенко
+ * @version 1.0
+ * Этот класс является демонстацией первого созданного проэкта на языке Java
+ */
+package ua.zp.brainacad;
+
+public class ItemOne {
+    public static void main(String[] args) {
+        System.out.println("This is first app in brainacademy");
+    }
+}
